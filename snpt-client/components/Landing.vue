f2312921bead8540f901ca9150cfb93c3bf7cc94
wadafacc <70700633+Wadafacc@users.noreply.github.com>
2022-03-07 14:12:08 +0100

    implemented basic landing page

---
+++ snpt-client/components/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <div class="landing flex">
+        <div class="logo">SNPT</div>
+        <div class="sub">F*#! Pastebin</div>
+        <a href="" class="arrow">&#8609;</a>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingComponent'
+}
+</script> 
+
+<style src="~/assets/styles/landingpage.css"></style>
---
+++ snpt-client/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="land">
+      <Landing/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script> 
+
+<style scoped>
+
+</style>
